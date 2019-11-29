7279ac2093692d7e218cf0b30f303539ffba9cce
hsicen <hsiceni@gmail.com>
2019-11-29 10:20:58 +0800

    二叉搜索树添加搜索删除操作

---
+++ core/src/main/java/com/hsicen/core/tree/Node.java
@@ -0,0 +1,17 @@
+package com.hsicen.core.tree;
+
+/**
+ * <p>作者：hsicen  2019/11/29 9:40
+ * <p>邮箱：codinghuang@163.com
+ * <p>作用：
+ * <p>描述：二叉树结点
+ */
+public class Node {
+    int data;
+    Node left;
+    Node right;
+
+    public Node(int data) {
+        this.data = data;
+    }
+}
