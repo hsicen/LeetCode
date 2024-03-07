205add5e40f04a039fa9d8f308b1ebef9740b005
hsicen <hsiceni@gmail.com>
2024-03-07 22:19:21 +0800

    [leetcode-80] 删除有序数组中的重复项 II.

--- core/src/main/java/com/hsicen/core/leetcode/Solution26.java
+++ core/src/main/java/com/hsicen/core/leetcode/Solution26.java
@@ -1,1 +1,34 @@
-package com.hsicen.core.leetcode;/** * @author: hsicen * @date: 2024/3/7 21:11 * @email: codinghuang@163.com * description: 26. 删除有序数组中的重复项 * <p> * 给你一个 非严格递增排列 的数组 nums ，请你 原地 删除重复出现的元素， * 使每个元素 只出现一次 ，返回删除后数组的新长度。元素的 相对顺序 应该保持 一致 。 * 然后返回 nums 中唯一元素的个数。 */public class Solution26 {  public static void main(String[] args) {    int[] nums = {0, 0, 1, 1, 1, 1, 2, 3, 4, 4};    int len = removeDuplicates(nums);    System.out.println(len);    for (int i = 0; i < len; i++) {      System.out.print(nums[i] + " ");    }  }  public static int removeDuplicates(int[] nums) {    int index = 0;    for (int i = 0; i < nums.length; i++) {      if (nums[i] != nums[index]) {        index++;        nums[index] = nums[i];      }    }    return index + 1;  }}
+package com.hsicen.core.leetcode;
+
+/**
+ * @author: hsicen
+ * @date: 2024/3/7 21:11
+ * @email: codinghuang@163.com
+ * description: 26. 删除有序数组中的重复项
+ * <p>
+ * 给你一个 非严格递增排列 的数组 nums ，请你 原地 删除重复出现的元素，
+ * 使每个元素 只出现一次 ，返回删除后数组的新长度。元素的 相对顺序 应该保持 一致 。
+ * 然后返回 nums 中唯一元素的个数。
+ */
+public class Solution26 {
+
+    public static void main(String[] args) {
+        int[] nums = {0, 0, 1, 1, 1, 1, 2, 3, 4, 4};
+        int len = removeDuplicates(nums);
+        System.out.println(len);
+        for (int i = 0; i < len; i++) {
+            System.out.print(nums[i] + " ");
+        }
+    }
+
+    public static int removeDuplicates(int[] nums) {
+        int index = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != nums[index]) {
+                index++;
+                nums[index] = nums[i];
+            }
+        }
+        return index + 1;
+    }
+}
---
+++ core/src/main/java/com/hsicen/core/leetcode/Solution80.java
@@ -0,0 +1,32 @@
+package com.hsicen.core.leetcode;
+
+/**
+ * @author: hsicen
+ * @date: 2024/3/7 21:44
+ * @email: codinghuang@163.com
+ * description: 80. 删除有序数组中的重复项 II
+ * <p>
+ * 给你一个有序数组 nums ,请你 原地 删除重复出现的元素，使每个元素 最多出现两次，返回删除后数组的新长度。
+ * 不要使用额外的数组空间，你必须在 原地 修改输入数组 并在使用 O(1) 额外空间的条件下完成。
+ */
+public class Solution80 {
+    public static void main(String[] args) {
+        int[] nums = {1, 1, 1, 1, 1, 2, 2, 3, 3, 4};
+        int len = removeDuplicates(nums);
+        System.out.println(len);
+        for (int i = 0; i < len; i++) {
+            System.out.print(nums[i] + " ");
+        }
+    }
+
+    public static int removeDuplicates(int[] nums) {
+        int count = 2;
+        for (int pos = 2; pos < nums.length; pos++) {
+            if (nums[pos] != nums[count - 2]) {
+                nums[count++] = nums[pos];
+            }
+        }
+
+        return count;
+    }
+}
