02ec40064e0386646c2b8342143a642df678d0f3
hsicen <hsiceni@gmail.com>
2024-03-10 20:50:09 +0800

    [leetcode-121] 买卖股票的最佳时机.

--- core/src/main/java/com/hsicen/core/leetcode/Solution121.java
+++ core/src/main/java/com/hsicen/core/leetcode/Solution121.java
@@ -5,10 +5,10 @@ package com.hsicen.core.leetcode;
  * 邮箱：codinghuang@163.com
  * 作用：
  * 描述：121. 买卖股票的最佳时机
- * <a href="https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock/">...</a>
  * <p>
- * 给定一个数组，它的第 i 个元素是一支给定股票第 i 天的价格。
- * 如果你最多只允许完成一笔交易（即买入和卖出一支股票一次），设计一个算法来计算你所能获取的最大利润。
+ * 给定一个数组，它的第 i 个元素是一支给定股票第 i 天的价格。
+ * 如果你最多只允许完成一笔交易（即买入和卖出一支股票一次），
+ * 设计一个算法来计算你所能获取的最大利润。
  * 注意：你不能在买入股票前卖出股票。
  */
 class Solution121 {
