f6b1a0df6e9e8d959a61d86c7258907164454557
hsicen <codinghuang@163.com>
2025-07-14 15:41:33 +0800

    [leetcode-242] 有效的字母异位词(HashMap统计).

---
+++ code/src/main/java/com/hsicen/code/hashmap/Solution242.kt
@@ -0,0 +1,44 @@
+package com.hsicen.code.hashmap
+
+/**
+ * @author: hsc
+ * @date: 2025/7/14 15:24
+ * @email: codinghuang@163.com
+ * @description: 242.有效的字母异位词
+ * 给定两个字符串 s 和 t ，编写一个函数来判断 t 是否是 s 的字母异位词。
+ * 注意：若 s 和 t 中每个字符出现的次数都相同，则称 s 和 t 互为字母异位词。
+ *
+ * 示例 1:
+ * 输入: s = "anagram", t = "nagaram"
+ * 输出: true
+ *
+ * 示例 2:
+ * 输入: s = "rat", t = "car"
+ * 输出: false
+ */
+object Solution242 {
+
+    fun isAnagram(s: String, t: String): Boolean {
+        HashMap<Char, Int>().apply {
+            s.forEach {
+                put(it, getOrDefault(it, 0) + 1)
+            }
+
+            t.forEach {
+                put(it, getOrDefault(it, 0) - 1)
+            }
+            return values.all { it == 0 }
+        }
+    }
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val s = "anagram"
+        val t = "nagaram"
+        println("$s $t isAnagram=${isAnagram(s, t)}")
+
+        val s1 = "rat"
+        val t1 = "car"
+        println("$s1 $t1 isAnagram=${isAnagram(s1, t1)}")
+    }
+}
